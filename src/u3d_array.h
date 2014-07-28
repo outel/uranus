aac3427b52a4c8cb5733671ff5d5c97835000d07
LinkWu <wjianl@qq.com>
2014-07-28 21:17:11 +0800

    imp array

--- src/u3d_array.h
+++ src/u3d_array.h
@@ -3,8 +3,16 @@
 
 
 typedef struct _U3DArray4float {
-	
+	float		*raw_data;
+	unsigned	lenght;
 } U3DArray4float, *U3DArray4float_ptr;
 
+
+
+
+typedef struct _U3DArray4unsigned {
+	unsigned	*raw_data;
+	unsigned	lenght;
+} U3DArray4unsigned, *U3DArray4unsigned_ptr;
 
 #endif
--- src/u3d_object.h
+++ src/u3d_object.h
@@ -3,12 +3,12 @@
 
 
 typedef struct _U3DObject {
-	float		*vertex_raw_data;
-	float		*vertex_raw_data4world;
-	float		*vertex_raw_data4camera;
-	float		*vertex_raw_data4final;
+	U3DArray4float		vertex_raw_data;
+	U3DArray4float		vertex_raw_data4world;
+	U3DArray4float		ertex_raw_data4camera;
+	U3DArray4float		vertex_raw_data4final;
 
-	unsigned	*index_raw_data;
+	U3DArray4unsigned	index_raw_data;
 
 	float		x, y, z;
 } U3DObject, *U3DObject_ptr;
